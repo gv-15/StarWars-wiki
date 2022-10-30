54855e08dba536821903809724534b5cd0b5f9e1
Gaizka <gaizkaf1ferrari@gmail.com>
2022-10-30 18:26:18 +0100

    feat: movie router works

--- src/app/components/movie/movie.component.ts
+++ src/app/components/movie/movie.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-movie',
   templateUrl: './movie.component.html',
-  styleUrls: ['./movie.component.scss']
+  styleUrls: ['./movie.component.scss'],
 })
 export class MovieComponent implements OnInit {
-
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute // eslint-disable-next-line @typescript-eslint/no-empty-function
+  ) {}
 
   ngOnInit(): void {
+    this.route.paramMap.subscribe((paramMap) => {
+      const id = paramMap.get('id');
+      console.log(id);
+    });
   }
-
 }
